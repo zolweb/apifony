e03b3f08f0f42de7027776d36598e7c8af08fe9b
Benjamin Favre <benjamin.favre@zol.fr>
2023-06-12 16:52:07 +0200

    sav

---
+++ symfony/src/Command/Bundle/ComposerJson.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command\Bundle;
+
+class ComposerJson implements File
+{
+    public function __construct(
+        private readonly string $packageName,
+        private readonly string $namespace,
+    ) {
+    }
+
+    public function getPackageName(): string
+    {
+        return $this->packageName;
+    }
+
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
+    public function getFolder(): string
+    {
+        return '';
+    }
+
+    public function getName(): string
+    {
+        return 'composer.json';
+    }
+
+    public function getTemplate(): string
+    {
+        return 'composer.json.twig';
+    }
+
+    public function getParametersRootName(): string
+    {
+        return 'composer';
+    }
+}
