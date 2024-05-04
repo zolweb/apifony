7ebe7af364cc93a17da41d9b714b6d4e5534f6f8
Benjamin Favre <benjamin.favre@zol.fr>
2024-05-04 13:47:31 +0200

    sav

--- tests/TestHandler.php
+++ tests/TestHandler.php
@@ -13,32 +13,36 @@ class TestHandler implements TagHandler
 {
     public function firstOperationFromSchemaPayloadToApplicationJsonContent(string $pPathParamString, float $pPathParamNumber, int $pPathParamInteger, bool $pPathParamBoolean, string $qQueryParamString, float $qQueryParamNumber, int $qQueryParamInteger, bool $qQueryParamBoolean, string $hHeaderParamString, float $hHeaderParamNumber, int $hHeaderParamInteger, bool $hHeaderParamBoolean, string $cCookieParamString, float $cCookieParamNumber, int $cCookieParamInteger, bool $cCookieParamBoolean, Schema $requestBodyPayload): FirstOperation200ApplicationJsonResponse
     {
-        return new FirstOperation200ApplicationJsonResponse(
-            new FirstOperation200ApplicationJsonResponsePayload(
-                json_encode([
-                    'pathParamString' => $pPathParamString,
-                    'pathParamNumber' => $pPathParamNumber,
-                    'pathParamInteger' => $pPathParamInteger,
-                    'pathParamBoolean' => $pPathParamBoolean,
-                    'queryParamString' => $qQueryParamString,
-                    'queryParamNumber' => $qQueryParamNumber,
-                    'queryParamInteger' => $qQueryParamInteger,
-                    'queryParamBoolean' => $qQueryParamBoolean,
-                    'headerParamString' => $hHeaderParamString,
-                    'headerParamNumber' => $hHeaderParamNumber,
-                    'headerParamInteger' => $hHeaderParamInteger,
-                    'headerParamBoolean' => $hHeaderParamBoolean,
-                    'cookieParamString' => $cCookieParamString,
-                    'cookieParamNumber' => $cCookieParamNumber,
-                    'cookieParamInteger' => $cCookieParamInteger,
-                    'cookieParamBoolean' => $cCookieParamBoolean,
-                    'requestBodyPayload' => $requestBodyPayload,
-                ]),
-            ),
-            'string',
-            .1,
-            1,
-            true,
-        );
+        try {
+            return new FirstOperation200ApplicationJsonResponse(
+                new FirstOperation200ApplicationJsonResponsePayload(
+                    json_encode([
+                        'pathParamString' => $pPathParamString,
+                        'pathParamNumber' => $pPathParamNumber,
+                        'pathParamInteger' => $pPathParamInteger,
+                        'pathParamBoolean' => $pPathParamBoolean,
+                        'queryParamString' => $qQueryParamString,
+                        'queryParamNumber' => $qQueryParamNumber,
+                        'queryParamInteger' => $qQueryParamInteger,
+                        'queryParamBoolean' => $qQueryParamBoolean,
+                        'headerParamString' => $hHeaderParamString,
+                        'headerParamNumber' => $hHeaderParamNumber,
+                        'headerParamInteger' => $hHeaderParamInteger,
+                        'headerParamBoolean' => $hHeaderParamBoolean,
+                        'cookieParamString' => $cCookieParamString,
+                        'cookieParamNumber' => $cCookieParamNumber,
+                        'cookieParamInteger' => $cCookieParamInteger,
+                        'cookieParamBoolean' => $cCookieParamBoolean,
+                        'requestBodyPayload' => $requestBodyPayload,
+                    ], JSON_THROW_ON_ERROR),
+                ),
+                'string',
+                .1,
+                1,
+                true,
+            );
+        } catch (\JsonException $e) {
+            throw new \RuntimeException('Unexpected invalid JSON');
+        }
     }
 }
