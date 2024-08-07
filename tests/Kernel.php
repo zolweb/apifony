18e1dc3df5859726921a7a621564de24c1835b36
Benjamin Favre <benjamin.favre@zol.fr>
2024-08-07 13:48:52 +0200

    sav

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -31,6 +31,13 @@ class Kernel extends \Symfony\Component\HttpKernel\Kernel
     {
         $container->extension('framework', [
             'http_method_override' => false,
+            'handle_all_throwables' => true,
+            'validation' => [
+                'email_validation_mode' => 'html5',
+            ],
+            'php_errors' => [
+                'log' => true,
+            ],
             'test' => true,
         ]);
 
